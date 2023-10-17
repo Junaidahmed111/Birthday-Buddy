6cf68b5d684590c1f3cfdd5f68b23a75977d200e
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-17 20:10:21 +0500

    working on cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Cards = () => {
   return (
     <section>
-      <div className="font-bold text-md">
-        <h3>
-          Wished <span className="bg-colorB">1 million</span> users worldwide
+      <div className="font-bold text-xl flex justify-center">
+        <h3 className="text-white">
+          Wished <span className="text-colorB">1 million</span> users worldwide
         </h3>
       </div>
     </section>
