ae2cc14cdc4be803958041af444f8ba183e0b3fe
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-17 12:59:16 +0500

    working on footer section

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header/>
         {children}
-        <Hero/>
-        <CTA/>
+        
+        
         </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import CTA from "@/components/CTA";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <Hero />
+      <CTA />
+    </main>
+  );
 }
