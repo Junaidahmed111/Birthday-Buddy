4a53bdd919b45b0330331b495450b776f4f37b34
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-21 18:31:28 +0500

    fixes

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -19,10 +19,10 @@ const Feature = () => {
 
   return (
     <section id="features" className="my-16">
-      <div className="text-white flex justify-center lg:py-10">
+      <div className="text-white flex justify-center lg:py-14">
         <h1 className="font-bold text-3xl lg:text-5xl">Features</h1>
       </div>
-      <div className="flex flex-col lg:flex-row gap-x-5 gap-y-5 w-full py-5 md:py-0 md:h-screen">
+    <div className="flex flex-col lg:flex-row gap-x-5 gap-y-5 w-full py-5 md:py-0 md:h-screen justify-evenly" >
         <div className="flex flex-row lg:flex-col items-center justify-center flex-1 lg:flex-[0.4] gap-x-4 pl-4 text-sm">
           {featureData.map((feature, index) => (
             <SingleFeature
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="w-full flex items-center justify-center text-center mt-10 lg:mt-0 lg:h-[90vh]">
       <div className=" text-white px-3 lg:p-12 lg:pt-16 flex items-center gap-y-5 flex-col w-full lg:w-[80%]">
         <h1 className=" text-5xl lg:text-7xl ">
-          Welcome to Your <br /> <span className="text-colorB">Birthday</span>{" "}
+          Welcome to Your <br /> <span className="text-colorB">Birthday</span>
           Buddy
         </h1>
         <p className=" text-lg lg:text-md lg:w-[60%]">
