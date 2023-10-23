5a667b06860a13fdc7674081882ec638a80ee780
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-23 23:01:46 +0500

    fix

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -39,7 +39,7 @@ const Feature = () => {
             <Image
               className="w-full md:max-lg:h-[50%] h-[100%] px-5 lg:px-0 rounded-[5rem] lg:!rounded-2xl"
               src={activeFeatureImgUrl}
-              width={200}
+              width={2000}
               height={2000}
               alt="Feature Image"
             />
